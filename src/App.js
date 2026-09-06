dc4a85a3884c8be578b4d4f4a88ae4da3e1c2fb3
Atif Muhammad Kamil <atif.m.kamil@gmail.com>
2026-09-06 21:39:05 +0700

    feat: add sign-in and dashboard pages with routes, redux store, and components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import { listen } from "./redux/listener";
+import { AppRoutes } from "./routes";
+
 function App() {
-  return <></>;
+  useEffect(() => {
+    listen();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import GuardRoute from "../components/GuardRoute";
+import GuestOnlyRoute from "../components/GuestOnlyRoute";
+
+import Signin from "../pages/signin";
+import SNavbar from "../components/Navbar";
+import { HomeRoute } from "./HomeRoute";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="signin"
+        element={
+          <GuestOnlyRoute>
+            <Signin />
+          </GuestOnlyRoute>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <>
+            <SNavbar />
+            <GuardRoute />
+          </>
+        }
+      >
+        <Route path="dashboard/*" element={<HomeRoute />} />
+        <Route path="" element={<Navigate to="/dashboard" replace={true} />} />
+      </Route>
+    </Routes>
+  );
+}
